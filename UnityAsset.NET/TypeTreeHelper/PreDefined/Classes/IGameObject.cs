eb82a28648bfe0c634be4503fc3ed19a990aa4a1
AXiX-official <axix@mail.ustc.edu.cn>
2025-10-20 12:09:06 +0800

    fix IGameObject

--- UnityAsset.NET/TypeTreeHelper/PreDefined/Classes/IGameObject.cs
+++ UnityAsset.NET/TypeTreeHelper/PreDefined/Classes/IGameObject.cs
@@ -1,11 +1,9 @@
 ﻿namespace UnityAsset.NET.TypeTreeHelper.PreDefined.Classes;
 
-public interface IGameObject : IAsset
+public interface IGameObject : INamedAsset
 {
     // need more example
     public List<IUnityType> m_Component { get; }
 
     public UInt32 m_Layer { get; }
-
-    public string m_Name { get; }
 }
